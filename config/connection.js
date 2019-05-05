e83c9d5755d492e2d9eae29d05bf802f263a5cf6
Sally Rae <sally.cardinale@gmail.com>
2019-05-04 20:13:02 -0700

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+//require("dotenv").config();
+const mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "webuser",
+    password: "UCR",
+    database: "burgers_db"
+  });
+}
+connection.connect();
+module.exports = connection;
